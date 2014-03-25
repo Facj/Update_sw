372b9d9c69692edc5ff980bca510a8709a2eb943
Facj <fa.castro.jul@gmail.com>
2014-03-25 16:32:45 +0200

    New options

--- update.sh
+++ update.sh
@@ -6,19 +6,22 @@ source /home/pi/git/update.cfg
 cd /home/pi/git/Repo1
 #source /home/fatima/Raspi_sw/func.cfg
 usage () {
-    echo -e "Automatic software updater for Raspberry Pi that provides secure features by default.\n"
-    echo -e "Usage:  bash update.sh [options] \n"
-    echo  "Options for disabling features:"
-    echo "-c			Disable compilation check"
-    echo "-m                    Disable dynamic update.If the processes are running they won't be killed"
-    echo "-k                    Disable key-authenticity check"
-    echo -e "-a                    Disable all checks\n"
-    echo "More options:"
-    echo "-f <minutes>   [m <minutes>] [h <hours>] [d <days>] "
-    echo -e "Change update-check frequency to the value given.It takes minutes by default"
-    echo "If the value is 0 automatic update check is disabled"
-    echo "-r			Rollback to previous version. what includes??"			
-    echo "-h        Help on the usage"
+	echo -e "Automatic software updater for Raspberry Pi that provides secure and dynamic features by default.\n"
+	echo -e "Usage:  bash update.sh [options] \n"
+	echo  "Options for disabling features:"
+	echo "-c			Disable compilation check."
+	echo "-d                      Disable dynamic update.If the processes are running they will not be subsituted."
+	echo "-k                      Disable key-authenticity check."
+	echo "-t                      Disable unittest check."
+	echo -e "-a                      Disable all checks.\n"
+	echo "More options:"
+	echo "-f <minutes>   [m <minutes>] [h <hours>] [d <days>] "
+	echo -e "Change update-check frequency to the value given.It takes minutes by default."
+	echo "If the value is 0 automatic update check is disabled."
+	echo "-r			  Rollback to previous version."
+	echo "-x                        Force dynamic update, even if not supported."
+	echo "-s                        Reject the update if not unittest is provided"			
+	echo "-h                        Help on the usage"
 }
 
 #Secure feature enabled by default
@@ -28,21 +31,30 @@ usage () {
 
 
 #Check parameters
-while getopts ":hcmarkf:" option; do
+while getopts ":hcdtarkxsf:" option; do
     case "$option" in
 	h)  usage
             exit 0 
             ;;
 	c)  compile=false
             ;;
-	m)  dynamic=false
+	d)  dynamic=false
             ;;
         k)  key_check=false
             ;;
+ 	t)  test=false
+            ;;
         a)  dynamic=false
             compile=false
-            key_check
+            key_check=false
+            test=false
+            force=false
+            require_test=false
 	    ;;
+	x)  forced=true
+            ;;
+	t)  require_test=true
+            ;;
         r)  rollback_update
             exit 1
 	    ;;
@@ -82,11 +94,15 @@ done
 echo "SOFTWARE UPDATE"
 date
 
+#Check if network connection is available and fix it if possible.
 bash ../check_wifi.sh
 if [ $? -ne 0 ]
 then
     exit 1
 fi
+
+#Check for updates of the update system and install them if available.
+#self_update
 
 check_available_update $key_check
 if [ $? -ne 0 ]
@@ -102,33 +118,47 @@ else
 	echo "git merge failed"
         update_failed $new_v "Merging conflict"  #Only if files are modified in Raspberry Pi?
         #return 1 
-    else
-	check_updated_files $new_v #Not necessary if you are not going to compile
-	echo ${changed_files[*]}
     fi
     
     #Compilation
     if $compile; then
+	check_updated_files $new_v #Not necessary if you are not going to compile
+	echo ${changed_files[*]}
     	compile_update $new_v
+
 	if [ $? -ne 0 ]; then 
         	echo "Compilation error"
         else
-            	#echo ${compiled_files[*]}
-
-                successful_update
-            	#Change running version
-	    	if $dynamic; then
-			change_running_version
-	    	fi
-	    
+		if $test;then
+			check_for_tests
+			if [ $? -ne 0 ]; then 
+				echo "Tests not available."
+				if $require_test; then
+					echo "Test required and not available. Send issue."
+                        	        exit 1
+				fi
+			else
+	        		echo "Tests available"		
+		        	run_tests
+				if [ $? -ne 0 ]; then
+					echo "Test failed. Issue sent"
+				        exit 1
+				fi
+			fi
+		fi
+	fi 
+                   
+        successful_update
+        #Change running version
+	if $dynamic; then
+	 	change_running_version
 	fi
+	    
     else
       successful_update
     fi
     
        
-	
-	
     
 fi
 
