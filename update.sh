adcb379be16217baf841ccfe85c4eac53e9daf55
Facj <fa.castro.jul@gmail.com>
2014-04-28 15:29:37 +0300

    v.4.18

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version="4.17"                 
+version="4.18"                  
 source func.cfg   #In Raspberry Pi it requires the whole path
 source update.cfg
 #source /home/pi/git/check_wifi.sh
